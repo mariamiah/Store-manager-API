8403abccdcd0ef3ac1915925055f1cf7dd5af1e0
= <=>
2018-10-16 13:12:27 +0300

    Add models folder[Delivers #161247558]

---
+++ api/models/product_model.py
@@ -0,0 +1,9 @@
+class Product:
+    def __init__(self, product_id, stock_quantity, price, product_name,
+                 date_added, date_modified):
+        self.product_id = product_id
+        self.stpck_quantity = stock_quantity
+        self.price = price
+        self.product_name = product_name
+        self.date_added = date_added
+        self.date_modified = date_modified
