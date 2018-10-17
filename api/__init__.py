0383cdf21c46cd8e93ec18db25e73157e94748d6
= <=>
2018-10-17 15:11:00 +0300

    Modify init file

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from api.views.product_views import product
+from api.views.saleRecord_views import sale
 
 app = Flask(__name__)
 app.register_blueprint(product)
+app.register_blueprint(sale)
