aa90f61902b941cb35df623715b88951ed567d40
= <=>
2018-10-20 14:55:17 +0300

    Modify init file

--- api/__init__.py
+++ api/__init__.py
@@ -4,10 +4,12 @@ from api.views.saleRecord_views import sale
 from api.views.user_views import user
 from flasgger import Swagger
 
+
 app = Flask(__name__)
 app.register_blueprint(product)
 app.register_blueprint(sale)
 app.register_blueprint(user)
+
 # Define a swagger template
 template = {
     "swagger": "2.0",
