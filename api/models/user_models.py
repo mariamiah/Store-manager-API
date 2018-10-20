a224a1a09c3e9f34196a7f5247dfcac823fa1924
= <=>
2018-10-20 14:59:06 +0300

    Modify user models[elivers #161247558]

--- api/models/user_models.py
+++ api/models/user_models.py
@@ -1,22 +1,15 @@
-class StoreEmployee(object):
-    def __init__(self, employee_id, employee_name, email, gender):
+class User(object):
+    def __init__(self, employee_id, employee_name, email, gender, username,
+                 password, role):
         self.employee_id = employee_id
         self.employee_name = employee_name
         self.email = email
         self.gender = gender
+        self.username = username
+        self.password = password
+        self.role = role
 
-
-class Admin(StoreEmployee):
-    def __init__(self, employee_id, employee_name, email, gender,
-                 adminUser_name, admin_password):
-        super().__init__(employee_id, employee_name, email, gender)
-        self.adminUser_name = adminUser_name
-        self.admin_password = admin_password
-
-
-class Attendant(StoreEmployee):
-    def __init__(self, employee_id, employee_name, email, gender,
-                 attendantUser_name, attendant_password):
-        super().__init__(employee_id, employee_name, email, gender)
-        self.attendantUser_name = attendantUser_name
-        self.attendant_password = attendant_password
+    def check_role(self):
+        if self.role == "admin":
+            return "Is_admin"
+        return "Is_attendant"
