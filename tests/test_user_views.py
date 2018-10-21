1a59c62e91ed085a1fb0a4e74903134db3af7fe6
= <=>
2018-10-21 14:20:46 +0300

    Add tests for the user views[Delivers #161365850]

---
+++ tests/test_user_views.py
@@ -0,0 +1,23 @@
+import unittest
+from api import app
+import json
+
+
+class TestUserViews(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_register_a_user(self):
+        # Tests that the user is registered
+        user_data = {
+            "employee_name": "sarah",
+            "gender": "female",
+            "username": "sara",
+            "password": "475677r",
+            "email": "sara@gmail.com",
+            "role": "admin"
+        }
+        response = self.client.post('/api/v1/users',
+                                    content_type='application/json',
+                                    data=json.dumps(user_data))
+        self.assertEqual(response.status_code, 201)
